feba6736b7297f996ebc62fe8f8b38c1c23a318a
Lucas Menezes <llucas007menezes@gmail.com>
2026-03-20 20:20:55 -0300

    Feat: estilização

--- app_tarefas/urls.py
+++ app_tarefas/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.lista_tarefa, name="home")
+    path("", views.lista_tarefa, name="home"),
+    path("concluir/<int:tarefa_id>/", views.concluir_tarefa, name="concluir_tarefa"),
+    path("deletar/<int:tarefa_id>/", views.deletar_tarefa, name="deletar_tarefa"),
 ]
--- app_tarefas/views.py
+++ app_tarefas/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Tarefa
 
 # Create your views here.
 def lista_tarefa(request):
+    if request.method == 'POST':
+        novo_titulo = request.POST.get('novo_titulo')
+        if novo_titulo:
+            Tarefa.objects.create(titulo=novo_titulo)
+        return redirect('home')
+
     minhas_tarefas = Tarefa.objects.all()
 
     contexto = {
@@ -10,3 +16,14 @@ def lista_tarefa(request):
     }
 
     return render(request, 'lista.html', contexto)
+
+def concluir_tarefa(request, tarefa_id):
+    tarefa = Tarefa.objects.get(id=tarefa_id)
+    tarefa.concluida = True
+    tarefa.save()
+    return redirect('home')
+
+def deletar_tarefa(request, tarefa_id):
+    tarefa = Tarefa.objects.get(id=tarefa_id)
+    tarefa.delete()
+    return redirect('home')
